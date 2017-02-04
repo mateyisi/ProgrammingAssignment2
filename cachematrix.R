fb5197610661c272b1dcbd41eb258e4b859292ca
mohau <mohau@mohau-mateyisi-laptop>
2017-02-04 10:50:53 +0000

    summited the modified version of the assigment code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#The first function, 'makeCacheMatrix' creates a special "vector"
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  
+  ## The function 'makeCacheMatrix' takes a squaremetrix as an input
+  ## and creates a vector whih is a specil list 
+  ##containing a functions:
+  ##          'setMat' to set the value of the matrix
+  ##          'get Mat' to get the value of the matrix
+  ##          'setMatInv' to set the value of the inverse
+  ##          'getMatInv' to get the value of the inverse
+  
+  ## The function returns the list of the functions
+  
+    Mat <- NULL
+    setMat <- function(y) {
+      x <<- y
+      Mat <<- NULL
+  }
+  getMat <- function() x
+  SetMatInv <- function(solve) Mat <<- solve
+  GetMatInv <- function() Mat
+  
+  list(setMat = setMat, getMat = getMat,
+       SetMatInv = SetMatInv,
+       GetMatInv = GetMatInv)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of of the special "matrix" 
+##created with the above function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  ## The function takes as na input a list 'x' of length 4 from the output
+  ## function ' makeCacheMatrix' it
+  ## Checks from the list if the inverse has been calculated if not, 
+  ## takes first element of the list, which the matrix used, to 
+  ## calculate the invierse and sets the value of the inverse in the cache via 
+  ## 'setMatInv'
+  
+  ## The function returns the inverse of the input matrix.
+  
+  m <- x$GetMatInv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  Matdata <- x$getMat()
+  m <- solve(Matdata, ...)
+  x$SetMatInv(m)
+  m
 }
